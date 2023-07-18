c4d9bd5c44082f263fc21e5e93d4d4afed21bd3c
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2023-07-19 01:10:21 +0300

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,44 +1,16 @@
-import io.circe.generic.semiauto.deriveCodec
-import io.circe.syntax._
-import io.circe.{Codec, Error, Json}
-import parser.{Data, WildberriesParser}
-import parser.dto.{CardInfoDto, ParserDto, RecDto}
-import sttp.client3._
-import sttp.client3.circe._
-import sttp.model.Uri
+import parser.{AliexpressParser, WildberriesParser}
+import parser.dto.StartDto
+
+
 
 object Main extends App {
+  val resultset = "catalog"
+  val query = "женщинам"
+  val sort = "popular"
 
- val categoryParseUrl: Uri = uri"https://search.wb.ru/exactmatch/ru/common/v4/search"
- val getRecommendedUrl: Uri = uri"https://waterfall-card-rec.wildberries.ru/api/v1/recommendations"
- val getCardInfoUrl: Uri = uri"https://card.wb.ru/cards/detail"
- val resultset = "catalog"
- val query = "женщинам"
- val sort = "popular"
+  val data =  StartDto(resultset, query, sort)
 
- val category = ParserDto(categoryParseUrl, query, sort, resultset)
+  WildberriesParser.start(data)
 
- val product = WildberriesParser.parseCategoriesPage(category)
-
- product.products.foreach { x =>
-   x.productIterator.foreach { value =>
-     println(s"${x.productElementName(x.productIterator.indexOf(value))} : $value")
-   }
-
-   println()
-
-   val rec = RecDto(getRecommendedUrl, x.id)
-
-   WildberriesParser.getRecommended(rec).foreach( x => {
-
-     val card = CardInfoDto(getCardInfoUrl, x.nm)
-
-     WildberriesParser.getCardInfo(card).map( x => {
-       x.productIterator.foreach { value =>
-         println(s"${x.productElementName(x.productIterator.indexOf(value))} : $value")
-       }
-       println()
-     })
-   })
- }
+  AliexpressParser.parseCategoriesPage()
 }
