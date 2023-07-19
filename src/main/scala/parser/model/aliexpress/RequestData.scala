35b18460956ae715ab35e1170cd91250a2e4d2bc
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2023-07-19 14:55:29 +0300

    Update RequestData.scala

--- src/main/scala/parser/model/aliexpress/RequestData.scala
+++ src/main/scala/parser/model/aliexpress/RequestData.scala
@@ -6,17 +6,41 @@ import jdk.incubator.vector.ByteVector
 
 import java.util.Base64
 
-case class RequestData(
+case class RequestData private (
                         catId: String,
                         g: String,
                         isGoldenItems: String, // Топ-товары ("n"/"y")
-                        searchInfo: String = "category:",
-                        decodedSearchInfo: String, // eyJ0aXRsZSI6ItCc0L7QsdC40LvRjNC90YvQtSDRgtC10LvQtdGE0L7QvdGLIiwgInNsdWciOiJtb2JpbGUtcGhvbmVzLWFjY2Vzc29yaWVzIiwgImlkIjoiMjQifQ==
+                        searchInfo: String,
                         isFavorite: String, // 4 звезды и более ("n"/"y")
                         source: String = "nav_category"
                       )
 
 case object RequestData{
+   def apply(
+              catId: String,
+              g: String,
+              isGoldenItems: String,
+              searchInfo: String,
+              isFavorite: String,
+  ): RequestData = {
+     val text = searchInfo
+       .replaceAll("\\s+:\\s+", ":")
+       .replaceAll("\\n+", "")
+       .replaceAll(",\\s+",", ")
+       .replaceAll("\\{\\s+","{")
+
+     val encodedSearchInfo = "category:" + Base64.getEncoder.encodeToString(text.getBytes("UTF-8"))
+
+     new RequestData(
+      catId=catId,
+      g=g,
+      isGoldenItems=isGoldenItems,
+      searchInfo=encodedSearchInfo,
+      isFavorite=isFavorite
+    )
+  }
+
   implicit val codecRequestData: Codec[RequestData] = deriveCodec[RequestData]
 }
+
 
