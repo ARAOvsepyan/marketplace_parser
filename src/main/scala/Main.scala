a547709b4be5128ce662b3a488fa7d4943e6875b
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2023-07-19 14:55:36 +0300

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,25 @@
-import parser.{AliexpressParser, WildberriesParser}
-import parser.dto.StartDto
+import parser.{AliexpressParser, Config, WildberriesParser}
+import parser.dto.{AliRequestDto, StartDto}
+import parser.model.aliexpress.{RequestData}
 
-
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 object Main extends App {
-  val resultset = "catalog"
-  val query = "женщинам"
-  val sort = "popular"
 
-  val data =  StartDto(resultset, query, sort)
+//  val data =  StartDto(Config.WB_RES_SET("catalog"), Config.WB_CAT("woman"), Config.WB_SORT("popular"))
 
-  WildberriesParser.start(data)
+//  WildberriesParser.start(data)
 
-  AliexpressParser.parseCategoriesPage()
+  val searchInfo = AliRequestDto(Config.AE_SEARCH_INFO_TITLE("mob_phone"), Config.AE_SEARCH_INFO_SLUG("mob_phone"), Config.AE_SEARCH_INFO_ID("mob_phone").toString)
+
+  val data = RequestData(
+    catId = "1",
+    g = "???",
+    isGoldenItems = "???",
+    searchInfo = searchInfo.asJson.toString() ,
+    isFavorite = "???"
+  )
+
+  println(AliexpressParser.parseCategoriesPage(data))
 }
